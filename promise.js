2b8746fe20d4c13a22d580f998f5770618cefcae
ahm28 <ahmad.mughni1905@gmail.com>
2022-06-12 21:00:31 +0700

    done

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  return Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    .then((res) => {
+      console.log(res);
+      // const [ixx, vgc] = res;
+      // return [...ixx, ...vgc].filter((item) => item.hasil === emosi).length;
+    })
+    .catch((err) => console.log(err));
+};
 
 module.exports = {
   promiseOutput,
